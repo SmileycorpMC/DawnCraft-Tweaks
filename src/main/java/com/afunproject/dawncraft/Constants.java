37343778e0f65a58e26c02550e4bd327f0baf1f0
Smileycorp <callum.j.longbottom@gmail.com>
2024-05-31 12:15:04 +0100

    remove rl mixin

--- src/main/java/com/afunproject/dawncraft/Constants.java
+++ src/main/java/com/afunproject/dawncraft/Constants.java
@@ -3,15 +3,10 @@ package com.afunproject.dawncraft;
 import net.minecraft.resources.ResourceLocation;
 
 public class Constants {
-
-	public static final String LEGACY_MODID = "afptweaks";
+	
 	public static final String MODID = "dawncraft";
 	public static String VERSION = "N/A";
 	public static final float DAMAGE_MULT = 0.5f;
-	
-	static {
-		if (DAMAGE_MULT == Math.floor(DAMAGE_MULT)) throw new RuntimeException("Modifed JAR");
-	}
 
 	public static ResourceLocation loc(String path) {
 		return new ResourceLocation(MODID, path);
--- src/main/java/com/afunproject/dawncraft/ProxyMod.java
+++
@@ -1,8 +0,0 @@
-package com.afunproject.dawncraft;
-
-import net.minecraftforge.fml.common.Mod;
-
-@Mod(Constants.LEGACY_MODID)
-public class ProxyMod {}//TODO: remove after quest giver removes dependency
-//quest giver still hasn't removed dependency despite the mod not using anything
-//doesn't look like it's going to now, so we're stuck loading a second mod with 0 content to make the game load
